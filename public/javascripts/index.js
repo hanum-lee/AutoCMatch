26a9a5201853e8d26e5058146a87ae081e5bffde
hanum-lee <chococloudy@gmail.com>
2019-03-16 11:36:56 -0600

    Totally forgot what I did

---
+++ public/javascripts/index.js
@@ -0,0 +1,10 @@
+console.log("TEsting");
+console.log("testingthe restart");
+
+let req = new XMLHttpRequest();
+let path = 'http://localhost:3000';
+
+
+req.onreadystatechange = function (e) {
+    if (req.readyState === XMLHttpRequest.DONE)
+};
